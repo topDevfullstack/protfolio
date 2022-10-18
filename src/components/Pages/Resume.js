eb9a77de07a46c8e383a8944da3764841fe9dfce
topDevfullstack <kalvinson961010@gmail.com>
2022-10-18 05:22:38 -0400

    Change resume-canada-noPhoto

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import "../css/resume.css";
-import resumePDF from "../resume/Kalvin Son Resume_Cananda.pdf";
+import resumePDF from "../resume/Kalvin Son Resume_Canada+no.pdf";
 
 const Outercontainer = styled.div`
     padding: 40px;
